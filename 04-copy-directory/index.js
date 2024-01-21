b38dd174ccd0ceb834c2b480a04d35a7ced7af61
Sergey Kruglov <serega-kolpino@inbox.ru>
2024-01-21 21:30:49 +0300

    fix: compleat task 04-copy-directory (fix del file)

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -3,19 +3,31 @@ const path = require("path")
 const filePath = path.join(__dirname, "files")
 const filePathNew = path.join(__dirname, "files-copy")
 
-fs.mkdir(filePathNew, { recursive: true }, (err) => {
+fs.mkdir(filePathNew, { recursive: true }, (err,files) => {
     if(err) {
         throw err
     }
+    console.log("папка создана")
 })
-fs.readdir(filePath, (error, files) => {  
+fs.readdir(filePathNew, (error, files) => {
     if (error) return console.log(error);
-    files.forEach((file) => {
-        fs.copyFile(path.join(__dirname, "files", file), path.join(__dirname, "files-copy", file), (err) => {
-            if (err) {
-                console.log("Error Found:", err);
-            }
+    for (const file of files) {
+        fs.unlink(path.join(__dirname, "files-copy", file), (err) => {
+          if (err) throw err;
         });
+    }
+    console.log("папка очищена")
+    fs.readdir(filePath, (error, files) => {  
+        if (error) return console.log(error);
+        files.forEach((file) => {
+            fs.copyFile(path.join(__dirname, "files", file), path.join(__dirname, "files-copy", file), (err) => {
+                if (err) {
+                    console.log("Error Found:", err);
+                }
+            });
+        });
+        console.log("файлы соззданы")
     });
-});
+})
+
 
